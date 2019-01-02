4053c3d7fac0342bad96122754e20ad2a5d5a9fb
Markus Säynevirta <25539916+msaynevirta@users.noreply.github.com>
2019-01-02 17:42:30 +0200

    Revert "Adding a function for fixing missing data in the main data JSON file" (wrong file)
    This reverts commit 1d2b4ddf6fc988e86e7f59fb70f30f6516db9e45.

--- fix_data.py
+++ fix_data.py
@@ -1,10 +1,11 @@
-def collect_data(data, size, areacode):
-    from parser import collect_data
-    return collect_data(data, size, areacode)
+from json import dump, JSONEncoder, JSONDecoder
+from os import remove
 
-def write_new_arr(data, size, areacode, list_to_write):
-    from parser import write_data
-    write_data(data, size, areacode, list_to_write)
+def collect_data(data, size, areacode1, areacode2):
+    from parser import collect_data
+    data_arr1 = collect_data(data, size, areacode1)
+    data_arr2 = collect_data(data, size, areacode2)
+    return data_arr1, data_arr2
 
 def merge(data, data_dir, report_dir, size):
     val_list = []
@@ -12,33 +13,45 @@ def merge(data, data_dir, report_dir, size):
     areacode1 = input('Please enter the first areacode: ')
     areacode2 = input('Please enter the second areacode: ')
     
-    lab1 = data["dataset"]["dimension"]["Alue"]["category"]["label"][areacode1] #find the labels for the given areacodes
+    lab1 = data["dataset"]["dimension"]["Alue"]["category"]["label"][areacode1]
     lab2 = data["dataset"]["dimension"]["Alue"]["category"]["label"][areacode2]
     print('Merging {:s} to {:s}'.format(lab1, lab2))
     
-    report_file = str(report_dir + 'merges.txt') #write a log entry
+    report_file = str(report_dir + 'merges.txt')
     with open(report_file, 'a') as f:
         f.write('Merged {:s} ({:s}) to {:s} ({:s})\n'.format(lab1, areacode1, lab2, areacode2))
     
-    data_arr1 = collect_data(data, size, areacode1)
-    data_arr2 = collect_data(data, size, areacode2)
+    data_arr1, data_arr2 = collect_data(data, size, areacode1, areacode2)
 
-    #merge fuction (replace nulls)
-
-    write_new_arr(data, size, areacode1, arr_to_write) #replace the original data associated with the first areacode with the merged data
-
-def fix(data, filename, size):
+def fix(data, filename, size): 
+    val_list = [] 
+    
     areacode = input('Please enter areacode: ')
     
-    raw_list = collect_data(data, size, areacode)
+    index = data["dataset"]["dimension"]["Alue"]["category"]["index"][areacode]
+    start = i = size[2]*index #find the starting position of the population data 
+ 
+    lab = data["dataset"]["dimension"]["Alue"]["category"]["label"][areacode]
 
-    print(raw_list)
-    val_list = raw_list[1]
+    while i < start + size[2]:
+        val = data["dataset"]["value"][i]
+        val_list.append(val)
+        i += 1
 
     print('Current data, please enter the fixed data as a list')
     print(val_list)
-
     val_list = [int(x) for x in input().split(', ')]
+    i = start
+    j = 0
 
-    write_data(data, size, areacode, val_list)
+    while i < start + size[2]:
+        data["dataset"]["value"][i] = val_list[j]
+        print(val_list[j])
+        i += 1
+        j += 1
 
+    print('Values written')
+
+    os.remove(filename)
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent = 5)
